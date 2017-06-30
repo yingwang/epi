d0cc93a68ee265d92fc59397aa84b331367f8421
Ying Wang <xingqiwang@gmail.com>
2017-06-30 23:27:01 +0200

    first commit

---
+++ epi/c9q1.cpp
@@ -0,0 +1,11 @@
+//
+//  c9q1.cpp
+//  epi
+//
+//  Created by Ying Wang on 2017-06-30.
+//  Copyright © 2017 Ying. All rights reserved.
+//
+
+// implement a stack with max API
+
+#include <stdio.h>
---
+++ epi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  epi
+//
+//  Created by Ying Wang on 2017-06-30.
+//  Copyright © 2017 Ying. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
